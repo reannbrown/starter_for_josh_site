76868880f64a9f6f6eb4fbdcc210e13a7fe5a325
reannbrown <79641664+reannbrown@users.noreply.github.com>
2021-03-10 22:00:00 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="container-fluid">
+    <!-- div for the main image to stretch s to the edge of the page-->
+
+    <div class="container">
+        <!-- div to hold the content in the middle of the page-->
+        <div class="row">
+            <!-- a row that gives us access to the BS columns-->
+            <div class="col-md-6 text-center footer-content">
+                <p> <?php bloginfo('name'); ?></p>
+
+                <p>JOSH WHITKIN</p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr”>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss”>
+
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin”>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt=“facebook”>
+
+
+            </div><!-- row-->
+        </div><!-- container -->
+    </div><!-- container-fluid-->
+
+</footer>
+<?php wp_footer(); ?>
