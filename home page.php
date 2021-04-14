2e3240465789bace50ddbe83681a4b8b4ed36260
reannbrown <79641664+reannbrown@users.noreply.github.com>
2021-04-14 15:18:56 +0800

    Create home page.php

---
+++ home page.php
@@ -0,0 +1,57 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            
+        
+	<!--placeholder for col 1 -->
+
+
+        <div class="blocker"></div>
+    </div> <!--      container -->
+
+</section>
+
+
+<section class="container-fluid articlebg">
+
+    <div class="container">
+        <div class="row">
+
+            <div>
+                <h3 class="text-center article-title"> LATEST ARTICLES </h3>
+            </div>
+
+            <div class="col-md-4 article-cent">
+
+	<!--placeholder for col 1 post id 102-->                      
+
+            </div> <!-- col-->
+
+
+            <div class="col-md-4 article-cent">
+
+	<!--placeholder for col 1 post id 104-->                                 
+              
+
+            </div> <!-- col-->
+
+
+            <div class="col-md-4 article-cent">
+
+	<!--placeholder for col 1 post id 106 -->
+            
+            </div> <!-- col-->
+
+
+        </div> <!-- row-->
+
+    </div> <!-- container-->
+
+</section>
+<?php get_footer();/* Tells WordPress to include footer.php */ ?>
