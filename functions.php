8dd3ee0fc15d9964ed66c9ecb9a46b6183351a95
reannbrown <79641664+reannbrown@users.noreply.github.com>
2021-04-14 20:37:40 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,6 +12,21 @@ add_theme_support( 'post-thumbnails' );
 ?>
 
 <?php
+function add_menu_link_class( $atts, $item, $args ) {
+if($args->link_class) {$atts['class'] = $args->link_class;
+}
+return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
+function add_menu_list_item_class($classes, $item, $args) {
+if($args->list_item_class){$classes[] = $args->list_item_class;
+                          }return $classes;
+    }
+add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
+ ?>
+
+
+<?php
 	if ( get_field(' headerbackgroundimage') ) {
 		echo 'style="background: url(' . get_field(' headerbackgroundimage') . ')"';
 	}
