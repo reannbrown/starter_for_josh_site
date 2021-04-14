92c098c422141b46fe38fc54784f9ca598e0c729
reannbrown <79641664+reannbrown@users.noreply.github.com>
2021-04-14 15:57:46 +0800

    Update home page.php

--- home page.php
+++ home page.php
@@ -10,7 +10,11 @@
             
         
 	<!--placeholder for aboutme -->
-    <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            
+<h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+<p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
 
 
         <div class="blocker"></div>
