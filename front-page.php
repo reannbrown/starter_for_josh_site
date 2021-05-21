718c95b49830ed87ca011af47228b0bcc3c1d654
reannbrown <79641664+reannbrown@users.noreply.github.com>
2021-05-21 13:52:22 +0800

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -6,8 +6,8 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?><section class="container-fluid aboutbg text-center">
     <div class="container">
 
-        <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
-        <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+        <h2 class="animated fadeInLeft"><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+        <p class="particle animated fadeInRight"><?php the_field('subtitleforaboutme'); ?></p>
         <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
         <div class="blocker"></div>
